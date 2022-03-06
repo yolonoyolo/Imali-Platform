dbf72be2d8c0ed9ef896b4c6c7e48c1da56b0294
Royce <rdemanou6@gmail.com>
2022-03-05 23:48:34 -0500

    FINAALLYYYY

--- app.py
+++ app.py
@@ -18,25 +18,25 @@ def login_required(f):
 
 # App config
 app = Flask(__name__, template_folder='templates') 
-# Session config
-# app.secret_key = "abc"
-# app.config['SESSION_COOKIE_NAME'] = 'google-login-session'
-# app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=5)
+Session config
+app.secret_key = "abc"
+app.config['SESSION_COOKIE_NAME'] = 'google-login-session'
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=5)
 
-# oAuth Setup
-# oauth = OAuth(app)
-# google=oauth.register(
-#     name='google',
-#     client_id="1005334554128-a10got2vmu0tlo420cit9lvkk402b4ml.apps.googleusercontent.com",
-#     client_secret="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",
-#     access_token_url='https://accounts.google.com/o/oauth2/token',
-#     access_token_params=None,
-#     authorize_url='https://accounts.google.com/o/oauth2/auth',
-#     authorize_params=None,
-#     api_base_url='https://www.googleapis.com/oauth2/v1/',
-#     userinfo_endpoint='https://openidconnect.googleapis.com/v1/userinfo',  # This is only needed if using openId to fetch user info
-#     client_kwargs={'scope': 'openid email profile'},
-# )
+oAuth Setup
+oauth = OAuth(app)
+google=oauth.register(
+    name='google',
+    client_id="1005334554128-a10got2vmu0tlo420cit9lvkk402b4ml.apps.googleusercontent.com",
+    client_secret="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",
+    access_token_url='https://accounts.google.com/o/oauth2/token',
+    access_token_params=None,
+    authorize_url='https://accounts.google.com/o/oauth2/auth',
+    authorize_params=None,
+    api_base_url='https://www.googleapis.com/oauth2/v1/',
+    userinfo_endpoint='https://openidconnect.googleapis.com/v1/userinfo',  # This is only needed if using openId to fetch user info
+    client_kwargs={'scope': 'openid email profile'},
+)
 
 
 @app.route("/")
