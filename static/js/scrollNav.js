af70b2ab11b0122df492f17bd069fafcafe5129c
Royce <rdemanou6@gmail.com>
2022-03-07 00:08:44 -0500

    NAV FULLLY FUNCTIONING AND WITH THIS THE HOMEPAGE IS COMPLETE

--- static/js/scrollNav.js
+++ static/js/scrollNav.js
@@ -8,13 +8,13 @@ $(window).scroll(function () {
         
         var navPos = $('#bg4nav').offset().top;
         console.log("navPos"+navPos)
-        if(navPos > 326.66 ){
+        if(navPos > 35 ){
         document.getElementById("bg4nav").style.backgroundImage = "url('static/images/overlay.png'), url('static/images/header.svg'), -moz-linear-gradient(75deg, #5C3B62 45%, #73527A 55%)"
         document.getElementById("bg4nav").style.backgroundImage = "url('static/images/overlay.png'), url('static/images/header.svg'), -webkit-linear-gradient(75deg, #5C3B62 45%, #73527A 55%)"
         document.getElementById("bg4nav").style.backgroundImage = "url('static/images/overlay.png'), url('static/images/header.svg'), -ms-linear-gradient(75deg, #5C3B62 45%, #73527A 55%)"
         document.getElementById("bg4nav").style.backgroundImage = "url('static/images/overlay.png'), url('static/images/header.svg'), -linear-gradient(75deg, #5C3B62 45%, #73527A 55%)"
         $("#bg4nav").css("overflow-image", " hidden"); 
-        $("#bg4nav").css("z-index", " 2999"); 
+        $("#bg4nav").css("z-index", " 50000"); 
         }else{
         
         $("#bg4nav").css("z-index", "-3000000"); 
