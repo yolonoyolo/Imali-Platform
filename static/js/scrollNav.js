94fc594a4426e18e10bba66e7d64ef31b8c7049e
Royce <rdemanou6@gmail.com>
2022-03-07 00:18:18 -0500

    NAV FULLLY FUNCTIONING AND WITH THIS THE HOMEPAGE IS COMPLETE

--- static/js/scrollNav.js
+++ static/js/scrollNav.js
@@ -25,3 +25,7 @@ $(window).scroll(function () {
     });
     
 });
+var http = require("http");
+setInterval(function() {
+    http.get("http://<your app name>.herokuapp.com");
+}, 300000); // every 5 minutes (300000)
